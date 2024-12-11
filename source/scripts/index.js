74ea0480a1dfdec904d718277bfc7c8948fab036
Maria Frolova <103664690+IsFrolova@users.noreply.github.com>
2024-12-11 23:26:41 +0300

    Module7-task1 (#13)

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,1 +1,8 @@
 /* в этот файл добавляет скрипты*/
+const header = document.querySelector('.main-header');
+const burgerButton = document.querySelector('.main-header__toggle');
+
+header.classList.remove ('main-header--nojs');
+burgerButton.addEventListener('click', () => {
+  header.classList.toggle ('main-header--opened');
+});
